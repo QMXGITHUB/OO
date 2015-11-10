d795c04c24b94b95a55b0fbe749f7e8164fb563f
mxqiao <mxqiao@thoughtworks.com>
2015-11-10 23:23:43 +0800

    [qmx] throw exception when same data with different unit.

--- OO/Length.cs
+++ OO/Length.cs
@@ -7,17 +7,10 @@ namespace OO
 {
     public class Length
     {
-        private double len;
-        private LengthUnit lengthUnit = LengthUnit.m;
+        double len;
         public Length(double length) 
         {
             this.len = length;
-        }
-
-        public Length(double length, LengthUnit lengthUnit)
-        {
-            this.len = length;
-            this.lengthUnit = lengthUnit;
         }
 
         public double GetLength() 
@@ -25,43 +18,12 @@ namespace OO
             return this.len; 
         }
 
-        public LengthUnit GetUnit()
-        {
-            return this.lengthUnit;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
         public bool Equal(Length length)
         {
-            if (len == length.GetLength())
-            {
-                if (lengthUnit == length.GetUnit())
-                {
-                    return true;
-                }
-                else
-                {
-                    throw new Exception("There is no defination about same length with diferent unit!!!");
-                }
-            }
-            else
-	        {
-                return false;
-	        }
+           return len == length.GetLength();
         }
 
 
 
-    }
-
-    public enum LengthUnit
-    {
-        m,
-        cm,
-        mm
     }
 }
--- OO/LengthTest.cs
+++ OO/LengthTest.cs
@@ -10,39 +10,47 @@ namespace OO
     public class LenghtTest
     {
         [Fact]
-        public void Varify_two_same_lengths_with_default_unit_m()
+        public void Given_2_same_lengths_return_same_length()
         {
             Length length1 = new Length(1);
             Length length2 = new Length(1);
-            bool isTrue = length1.Equal(length2);
-            Assert.True(isTrue);
+            bool actual_result = length1.Equal(length2);
+            Assert.True(actual_result);
         }
 
         [Fact]
-        public void Varify_two_different_lengths_with_default_unit_m()
+        public void Given_2_different_lengths_return_different_length()
         {
             Length length1 = new Length(1);
             Length length2 = new Length(2);
-            bool idFalse = length1.Equal(length2);
-            Assert.False(idFalse);
+            bool actual_result = length1.Equal(length2);
+            Assert.False(actual_result);
         }
 
         [Fact]
-        public void Varify_two_same_lengths_with_same_unit_cm()
+        public void Given_2_same_lengths_with_same_unit_m_return_same_length()
         {
-            Length length1 = new Length(1, LengthUnit.cm);
-            Length length2 = new Length(1, LengthUnit.cm);
-            bool isTrue = length1.Equal(length2);
-            Assert.True(isTrue);
+            double length1 = 1;
+            string unit1 = "m";
+            double length2 = 1;
+            string unit2 = "m";
+            string actual_result = Compare_Two_Lengths(length1, length2,unit1,unit2);
+            Assert.Equal("same length",actual_result);
         }
 
-        [Fact]
-        public void Verify_two_different_lengths_with_same_unit_mm()
+        private string Compare_Two_Lengths(double length1, double length2, string unit1, string unit2)
         {
-            Length length1 = new Length(1, LengthUnit.mm);
-            Length length2 = new Length(2, LengthUnit.mm);
-            bool isFalse = length1.Equal(length2);
-            Assert.False(isFalse);
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void Given_2_different_lengths_with_same_unit_mm_return_same_length()
+        {
+            double length1 = 1;
+            string unit1 = "mm";
+            double length2 = 2;
+            string unit2 = "mm";
+            string actual_result = Compare_Two_Lengths(length1, length2, unit1, unit2);
+            Assert.Equal("different length", actual_result);
         }
 
 
