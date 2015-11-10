b887e03938f913ca9b5adfbcda4bfb1d835ec9fc
mxqiao <mxqiao@thoughtworks.com>
2015-11-10 14:31:25 +0800

    assert two length, assert two lenght with same unit

---
+++ OO/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OO")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5369a5d7-015c-4711-aae0-56c1ad15c69e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OO/test_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using Xunit;
+
+namespace OO
+{
+    public class test_1
+    {
+        private string same_length = "same length";
+        private String different_length = "different_length";
+
+        [Fact]
+        public void Given_2_same_lengths_return_same_length()
+        {
+            double length1 = 1;
+            double length2 = 1;
+            string actual_result = Compare_Two_Lengths(length1, length2,"m","m");
+            Assert.Equal("same length",actual_result);
+        }
+
+        [Fact]
+        public void Given_2_different_lengths_return_different_length()
+        {
+            double length1 = 1;
+            double length2 = 2;
+            string actual_result = Compare_Two_Lengths(length1, length2,"m","m");
+            Assert.Equal("different length", actual_result);
+        }
+
+        [Fact]
+        public void Given_2_same_lengths_with_same_unit_m_return_same_length()
+        {
+            double length1 = 1;
+            string unit1 = "m";
+            double length2 = 1;
+            string unit2 = "m";
+            string actual_result = Compare_Two_Lengths(length1, length2,unit1,unit2);
+            Assert.Equal("same length",actual_result);
+        }
+        [Fact]
+        public void Given_2_different_lengths_with_same_unit_mm_return_same_length()
+        {
+            double length1 = 1;
+            string unit1 = "mm";
+            double length2 = 2;
+            string unit2 = "mm";
+            string actual_result = Compare_Two_Lengths(length1, length2, unit1, unit2);
+            Assert.Equal("different length", actual_result);
+        }
+
+        private string Compare_Two_Lengths(double length1, double length2,string unit1,string unit2)
+        {
+            if(length1==length2)
+                 return "same length";
+            else
+            {
+                return "different length";
+            }
+        }
+    }
+}
